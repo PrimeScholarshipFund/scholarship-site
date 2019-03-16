799af96d1c7890ac57dfa5779b3265c558eeeb22
llenk <ellenkeal@gmail.com>
2019-03-16 11:58:49 -0500

    made new and edit contacts form work

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,17 @@
 class ContactsController < ApplicationController
   before_action :load_contact_form, only: %i[edit update]
   def new
+    @contact = Contact.new
     render
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to root_path
+    else
+      redirect_to new_contact_path
+    end
   end
 
   def edit
@@ -9,14 +19,20 @@ class ContactsController < ApplicationController
   end
 
   def update
-    updated_contact = params[:contacts]
-    @contact.update(first_name: updated_contact[:first_name], last_name: updated_contact[:last_name], middle_initial: updated_contact[:middle_initial], address_line_1: updated_contact[:address_line_1], address_line_2: updated_contact[:address_line_2], city: updated_contact[:city], state: updated_contact[:state], zip_code: updated_contact[:zip_code], phone_number: updated_contact[:zip_code])
-    redirect_to root_path
+    if @contact.update(contact_params)
+      redirect_to root_path
+    else
+      redirect_to edit_contact_path(@contact.id)
+    end
   end
 
   private
 
   def load_contact_form
-    @contact = Contacts.find(params['id'])
+    @contact = Contact.find(params['id'])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :middle_initial, :address_line_1, :address_line_2, :city, :state, :zip_code, :phone_number)
   end
 end
--- app/models/Contacts.rb
+++
@@ -1,3 +0,0 @@
-class Contacts < ApplicationRecord
-
-end
